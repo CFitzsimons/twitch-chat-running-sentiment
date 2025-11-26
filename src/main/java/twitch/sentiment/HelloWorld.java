63f5552c316fba7ea0db9e24411444660d056557
CFitzsimons <colin@cleancodecompany.com>
2025-11-26 00:14:29 +0000

    chore: remove hello world

--- src/main/java/twitch/sentiment/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package twitch.sentiment;
-
-public class HelloWorld {
-    public static void main(String[] args) throws Exception {
-        System.out.println("HELLO FROM RUN CONFIG");
-        Thread.sleep(5000);
-        System.out.println("GOODBYE FROM RUN CONFIG");
-    }
-}
